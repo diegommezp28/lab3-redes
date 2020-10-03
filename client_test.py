40f37f98b1eb2b422815aff8ba47ced69c2541de
Diego <da.gomezp@uniandes.edu.co>
2020-10-02 22:08:17 -0500

    test

--- client_test.py
+++ client_test.py
@@ -11,14 +11,18 @@ video1 = open("./save_content/video1.mkv", "a+")
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(b'Hello, world')
-    data = s.recv(1024)
+    data = s.recv(4096)
     print('Received', repr(data))
 
+    i = 0
     while True:
         l = s.recv(4096)
-
+        i+=1
+        print(i)
         while l:
             video1.write(l)
             l = s.recv(4096)
+            i += 1
+            print(i)
         break
     s.close()
--- server_test.py
+++ server_test.py
@@ -9,15 +9,19 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen(1)
     conn, addr = s.accept()
+
+    i = 0
     with conn:
         print('Connected by', addr)
 
         l = video1.read(4096)
         while True:
-            data = conn.recv(1024)
-
+            data = conn.recv(4096)
+            print('data rcvd')
             while l:
                 s.send(l)
+                i += 1
+                print(i)
                 l = video1.read(4096)
             break
         conn.close()
