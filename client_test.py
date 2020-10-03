be707dbeea3f29fa43aab84834439a626ead5153
Diego <da.gomezp@uniandes.edu.co>
2020-10-02 21:59:34 -0500

    Send file

--- client_test.py
+++ client_test.py
@@ -2,9 +2,23 @@
 import socket
 
 HOST = '192.168.1.133'    # The remote host
-PORT = 50007              # The same port as used by the server
+PORT = 50007  # The same port as used by the server
+
+video1 = open("./save_content/video1.mkv", "a+")
+
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(b'Hello, world')
     data = s.recv(1024)
-print('Received', repr(data))
+    print('Received', repr(data))
+
+    while True:
+        l = s.recv(4096)
+
+        while l:
+            video1.write(l)
+            l = s.recv(4096)
+        break
+    s.close()
--- server_test.py
+++ server_test.py
@@ -1,14 +1,26 @@
 import socket   
 
 HOST = ''                 # Symbolic name meaning all available interfaces
-PORT = 50007              # Arbitrary non-privileged port
+PORT = 50007  # Arbitrary non-privileged port
+
+video1 = open("./video1.mkv", "rb")
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen(1)
     conn, addr = s.accept()
     with conn:
         print('Connected by', addr)
+
+        l = video1.read(4096)
         while True:
             data = conn.recv(1024)
-            if not data: break
-            conn.sendall(data)
+
+            while l:
+                s.send(l)
+                l = video1.read(4096)
+            break
+        conn.close()
+    s.close()
+            # if not data: break
+            # conn.sendall(data)
