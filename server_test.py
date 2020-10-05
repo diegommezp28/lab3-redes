b05b6969391609d6e9cfb1e3ac4a0c808bf5a9f1
diegommezp28 <da.gomezp@uniandes.edu.co>
2020-10-05 22:20:53 +0000

    sleep

--- server_test.py
+++ server_test.py
@@ -1,5 +1,6 @@
 import socket
 import hashlib
+import time
 
 HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 50007  # Arbitrary non-privileged port
@@ -14,7 +15,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     i = 0
     with conn:
         print('Connected by', addr)
-        conn.send(b'OK')
+        conn.sendall(b'OK')
         while True:
             data = conn.recv(4096)
             if data==b'Listo para recibir':
@@ -22,17 +23,18 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 l = video1.read(4096)
                 print(l)
                 while len(l)>0:
-                    conn.send(l)
+                    conn.sendall(l)
                     hashing.update(l) 
                     i += 1
                     print(i)
                     l = video1.read(4096)                
                 
-                
-                conn.send(b'hash')
+                time.sleep(0.1)
+                conn.sendall(b'hash')
                 hash = hashing.hexdigest()
-                print(f'Sent Hash: {hash}')                
-                conn.send(str.encode(hash))
+                print(f'Sent Hash: {hash}')
+                time.sleep(0.03)                
+                conn.sendall(str.encode(hash))
                 
         conn.close()
     s.close()
