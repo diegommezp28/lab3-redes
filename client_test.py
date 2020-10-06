5437210ff6c25b54124c9cfcd8c1ea1463747c84
Isabela <im.sarmieento@uniandes.edu.co>
2020-10-05 22:09:11 -0500

    Estructura básica del log que detecta si no termina

--- client_test.py
+++ client_test.py
@@ -6,9 +6,6 @@ HOST = 'localhost'    # The remote host
 PORT = 50007  # The same port as used by the server
 
 
-
-
-
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     data = s.recv(4096)
@@ -17,25 +14,23 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.sendall(b'Listo para recibir')
 
     i = 0
-    recibido=True
+    recibido = True
     while recibido:
         with open('./save_content/video1.mkv', 'wb') as video1:
             l = s.recv(4096)
-            i+=1
+            i += 1
             print(l)
             while l:
                 video1.write(l)
-            
+
                 l = s.recv(4096)
-            
+
                 i += 1
-            
-                print(i)
+
+                # print(i)
                 if l == b'hash':
                     break
 
-                      
-         
         hash = s.recv(4096)
 
         hasher = hashlib.new('sha256')
@@ -44,18 +39,16 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             while len(buffer) > 0:
                 hasher.update(buffer)
                 buffer = video.read(4096)
-        hashC=hasher.hexdigest()
+        hashC = hasher.hexdigest()
         print(f'Received Hash: {hash.decode()} and Calculated Hash: {hashC}')
-        if hashC==hash.decode():
+        if hashC == hash.decode():
             s.sendall(b'Recibido correctamente')
-            recibido=False
+            recibido = False
         else:
             s.sendall(b'Recibido incorrectamente')
 
-
-
         break
-    
+
     s.close()
 
 # with open("./save_content/video1.mkv", "rb") as video:
