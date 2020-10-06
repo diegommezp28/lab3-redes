e072f935629ed9412650bbea473feefee2a5da97
Isabela <im.sarmieento@uniandes.edu.co>
2020-10-06 15:16:11 -0500

    Multitread con interfaz

--- client_test.py
+++ client_test.py
@@ -2,52 +2,52 @@
 import socket
 import hashlib
 
-HOST = '192.168.1.133'    # The remote host
+HOST = 'localhost'  # '192.168.1.133'    # The remote host
 PORT = 50007  # The same port as used by the server
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     data = s.recv(4096)
-    print('Received', repr(data))
-
-    s.sendall(b'Listo para recibir')
-
-    i = 0
-    recibido = True
-    while recibido:
-        with open('./save_content/video1.mkv', 'wb') as video1:
-            l = s.recv(4096)
-            i += 1
-            # print(l)
-            while l:
-                video1.write(l)
-
+    if data == b'OK':
+        recibido = True
+        print('Recibí', repr(data))
+        s.sendall(b'Listo para recibir')
+        paquetes = 0
+        if recibido:
+            with open('./save_content/archivo_prueba', 'wb') as archivo:
                 l = s.recv(4096)
+                i = 1
+                # print(l)
+                while l:
+                    archivo.write(l)
 
-                i += 1
+                    l = s.recv(4096)
 
-                # print(i)
-                if l == b'hash':
-                    print('Hash flag')
-                    break
+                    i += 1
 
-        hash = s.recv(4096)
+                    # print(i)
+                    if l == b'hash':
+                        print('Hash flag')
+                        break
 
-        hasher = hashlib.new('sha256')
-        with open('./save_content/video1.mkv', 'rb') as video:
-            buffer = video.read(4096)
-            while len(buffer) > 0:
-                hasher.update(buffer)
+            hash = s.recv(4096)
+
+            hasher = hashlib.new('sha256')
+            with open('./save_content/archivo_prueba', 'rb') as video:
                 buffer = video.read(4096)
-        hashC = hasher.hexdigest()
-        print(f'Received Hash: {hash.decode()} and Calculated Hash: {hashC}')
-        if hashC == hash.decode():
-            s.sendall(b'Recibido correctamente')
-            print('Recibido correctamente')
-        else:
-            s.sendall(b'Recibido incorrectamente')
-        s.sendall(b'Paquetes recibidos')
+                while len(buffer) > 0:
+                    hasher.update(buffer)
+                    buffer = video.read(4096)
+            hashC = hasher.hexdigest()
+            print(
+                f'Received Hash: {hash.decode()} and Calculated Hash: {hashC}')
+            if hashC == hash.decode():
+                s.sendall(b'Recibido correctamente')
+                print('Recibido correctamente')
+            else:
+                s.sendall(b'Recibido incorrectamente')
+            s.sendall(b'Paquetes recibidos')
     s.close()
 #
 # with open("./save_content/video1.mkv", "rb") as video:
--- server_test_threads.py
+++ server_test_threads.py
@@ -1,71 +1,74 @@
 from threading import Thread
 from socket import socket
 import hashlib
+import time
+import server_log
+from server_view import preguntar
 
-HOST = 'localhost'                 # Symbolic name meaning all available interfaces
-PORT = 50007  # Arbitrary non-privileged port
+HOST = 'localhost'  # IP de enlace
+PORT = 50007        # Puerto de conección
 
 
 class Client(Thread):
-        
-    def __init__(self, conn, addr):
+
+    def __init__(self, conn, addr, file_to_send):
         # Inicializar clase padre.
-        Thread.__init__(self)        
+        Thread.__init__(self)
         self.conn = conn
         self.addr = addr
-    
+        self.file_to_send = file_to_send
+
     def run(self):
-        i=0
-        self.conn.send(b'OK')        
+        global faltan
+        i = 0
+        self.conn.send(b'OK')
         data = self.conn.recv(4096)
-        if data==b'Listo para recibir':
-            recibido = True
-            while recibido:
-                with open('./video1.mkv', 'rb') as video1:
-                    hashing = hashlib.new('sha256')                
-                    l = video1.read(4096)
-                    while len(l)>0:
-                        self.conn.send(l)
-                        hashing.update(l) 
-                        print(i)
-                        i += 1
-                        l = video1.read(4096)                
-                self.conn.send(b'hash')
-                hash = hashing.hexdigest()
-                print(f'Sent Hash: {hash}')                
-                self.conn.send(str.encode(hash))
-                rec =self.conn.recv(4096)
-                if rec == b'Recibido correctamente' :
-                    recibido=False
-                else:
-                    recibido=True
-        
+        if data == b'Listo para recibir':
+            print('Esperando...')
+            while(faltan != 0):
+                continue
+            print('Emepezando Trasnferencia')
+            with open('./'+self.file_to_send, 'rb') as file1:
+                hashing = hashlib.new('sha256')
+                l = file1.read(4096)
+                while len(l) > 0:
+                    self.conn.send(l)
+                    hashing.update(l)
+                    print(i)
+                    i += 1
+                    l = file1.read(4096)
+            time.sleep(0.1)
+            self.conn.send(b'hash')
+            hash = hashing.hexdigest()
+            print(f'Hash enviado: {hash}')
+            time.sleep(0.03)
+            self.conn.send(str.encode(hash))
+            rec = self.conn.recv(4096)
+            if rec == b'Recibido correctamente':
+                print('Recibido correctamente')
+            else:
+                print('Recibido incorrectamente')
 
 
 def main():
     s = socket()
-    
-    # Escuchar peticiones en el puerto 6030.
+
     s.bind((HOST, PORT))
     s.listen(0)
-    
-    while True:
-        conn, addr = s.accept()
-        c = Client(conn, addr)
-        c.start()
-        print("%s:%d se ha conectado." %addr)
+    connected = 0
+    terminar, num_con, file_to_send = preguntar()
+    global faltan
+    if not terminar:
+        while True:
+            conn, addr = s.accept()
+            connected += 1
+            faltan = num_con - connected
+            c = Client(conn, addr, file_to_send)
+            c.start()
+            print("%s:%d se ha conectado." % addr)
+            print("Faltan " + str(faltan) + " conexiones")
+    print('Apagando')
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
