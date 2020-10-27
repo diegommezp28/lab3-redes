34478cd5d16d5f699a38d86ff035bb4ddd8bc6f2
Isabela <im.sarmieento@uniandes.edu.co>
2020-10-26 21:57:34 -0500

    Base de UDP sencillo

---
+++ UDP/client_slow.py
@@ -0,0 +1,90 @@
+# Echo client program
+import hashlib
+import sys
+import time
+import datetime
+import socket
+import pickle
+import select
+import signal
+import os
+import timeit
+import threading
+
+from packet import *
+
+HOST = 'localhost'  # '192.168.2.133'    # The remote host
+PORT = 7735  # The same port as used by the server
+folder = './save_content/'
+bufsize = 4096
+
+
+def bytes_of(s):
+    return len(s.encode('utf-8'))
+
+
+class Client:
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.connect((HOST, PORT))
+        # self.sock.setblocking(0)
+        self.expected_seq = 0
+        self.ack_seq = 0
+        self.timeout = 0.1
+        self.packet_loss = False
+
+    def receive(self):
+        print("Soy un cliente")
+        global data
+        self.sock.send(b'Hola servidor')
+        # Recibe el nombre del archivo
+        data, address = self.sock.recvfrom(bufsize)
+        file_sended = repr(data).replace("b'", '').replace("'", "")
+        print('Esperando archivo ', file_sended)
+
+        data, address = self.sock.recvfrom(bufsize)
+        file = open('./save_content/'+file_sended, 'wb')
+        i = 0
+        while(data != b'Empezando a transmitir'):
+            continue
+        print('Empezando recepción')
+        try:
+            while True:
+                try:
+                    data, address = self.sock.recvfrom(bufsize)
+                    data = pickle.loads(data)
+                    if self.expected_seq != int(data.sequenceNumber, 2):
+                        continue
+
+                except socket.error:
+                    continue
+                except KeyboardInterrupt:
+                    self.sock.close()
+                    sys.exit(0)
+
+                file.write(data.packet)
+                # manda el ack del que recibio
+                sendACK = Acknowledgment(data.sequenceNumber)
+                self.sock.send(pickle.dumps(sendACK))
+                if data.eof == 1:
+                    print('File Received.')
+                    sys.exit(0)
+                self.expected_seq += 1
+            file.close()
+        except Exception as e:
+            print('Exception', e)
+            pass
+
+
+def main():
+    # Arguments are passed in order of acceptance in the command line arguments
+    client = Client()
+    # try:
+    client.receive()
+    #print("Run Time:", timeit.timeit(client.receive(), number=1))
+    # except:
+    #   print("Error occured while sending")
+
+
+if __name__ == "__main__":
+    main()
---
+++ UDP/server_slow.py
@@ -0,0 +1,161 @@
+from threading import Thread
+import socket
+from datetime import datetime
+import hashlib
+import time
+import sys
+from packet import *
+import sys
+import time
+import pickle
+import select
+import signal
+import os
+import timeit
+import threading
+
+
+from server_log import log
+from server_view import preguntar
+
+HOST = ''  # 'localhost' IP de enlace
+PORT = 7735        # Puerto de conección
+windowSize = 1
+mss = 3900
+
+
+def bytes_of(s):
+    return len(s.encode('utf-8'))
+
+
+class Client(Thread):
+
+    def __init__(self, sock, address, file_to_send, file_size):
+        # Inicializar clase padre.
+        Thread.__init__(self)
+        self.sock = sock
+        self.address = address
+        self.file_to_send = file_to_send
+        self.file_size = file_size
+        self.mss = mss
+        self.packetList = list()  # Packet List as buffer to save data as the packet to be sent
+        self.window_end = windowSize
+        self.unacked = 0
+        self.total_unacked = 0
+        self.timeout = 0.1  # Timeout value for every packet to be sent
+        self.sequenceNumber = 0  # default sequence Number to divide file
+
+    def divideFile(self, mss, filename, sequenceNumber):
+        #k = list()
+        print('Empieza a dividir ' + filename)
+        sequenceNumber = format(sequenceNumber, '032b')
+        with open(filename, "rb") as binary_file:
+            # Read the whole file at once
+            data = binary_file.read()
+            # Seek position and read N bytes
+            i = 0
+            length = sys.getsizeof(data)
+            while i <= length:
+                binary_file.seek(i)  # Go to beginning
+                couple_bytes = binary_file.read(mss)
+                if i + mss > length:
+                    # adding eof value = 1 for the lastpacket
+                    self.packetList.append(
+                        Packet(sequenceNumber, couple_bytes, 1))
+                else:
+                    self.packetList.append(
+                        Packet(sequenceNumber, couple_bytes, 0))
+                i += mss
+                temp = int(sequenceNumber, 2) + 1
+                sequenceNumber = format(temp, '032b')
+            print('Termina de dividir ' + filename)
+        return self.packetList
+
+    def send(self):
+        sendingData = self.divideFile(
+            self.mss, self.file_to_send, self.sequenceNumber)
+        print('Empieza a enviar')
+        self.unacked = 0
+        self.total_unacked = 0
+        while self.unacked < len(sendingData):
+            if self.total_unacked < self.window_end and (self.total_unacked + self.unacked) < len(sendingData):
+                # Cuando estoy adentro de la ventana, mando el ultimo que no he enviado
+                # print('Caso1')
+                for i in sendingData:
+                    sq = int(i.sequenceNumber, 2)
+                    if sq == self.total_unacked + self.unacked:
+                        sendingPkt = i
+                self.sock.sendto(pickle.dumps(sendingPkt), self.address)
+                self.total_unacked += 1
+                continue
+            else:
+                # print('Caso2')
+                # Cuando me pongo a revisar si si están recibiendo mis mensajes
+                ready = select.select([self.sock], [], [], self.timeout)
+                if ready[0]:
+                    ackData, address = self.sock.recvfrom(4096)
+                    if(address == self.address):
+                        ackData = pickle.loads(ackData)
+                        if ackData.ackField != 0b1010101010101010:
+                            continue
+                        if int(ackData.sequenceNumber, 2) == self.unacked:
+                            self.unacked += 1
+                            self.total_unacked -= 1
+                        else:
+                            self.total_unacked = 0
+                            continue
+                    else:
+                        self.total_unacked = 0
+                        continue
+                else:
+                    print('Timeout, sequence number = ', self.unacked)
+                    self.total_unacked = 0
+                    continue
+
+        print('Files are transmitted successfully.')
+        self.sock.close()
+
+    def run(self):
+        global faltan
+        i = 0
+        benviados = 0
+        # Envía el nombre del archivo
+        self.sock.sendto(
+            bytes(self.file_to_send, encoding='utf-8'), self.address)
+        while(faltan != 0):
+            continue
+        self.sock.sendto(b'Empezando a transmitir', self.address)
+        print('Empieza a transmitir')
+        self.send()
+
+
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((HOST, PORT))
+    # s.setblocking(0)
+    print('Servidor en: ', HOST, PORT)
+    connected = 0
+    terminar, num_con, file_to_send, file_size = preguntar()
+    global faltan
+    faltan = num_con
+    direcciones = {}
+    if not terminar:
+        while faltan != 0:
+            print('Esperando...')
+            data, address = s.recvfrom(mss)
+            dir_pu = address[0]+str(address[1])
+            ya_conectado = False if not direcciones.get(
+                dir_pu, False) else True
+            if(data == b'Hola servidor' and not ya_conectado):
+                direcciones[dir_pu] = True
+                connected += 1
+                faltan = num_con - connected
+                c = Client(s, address, file_to_send, file_size)
+                c.start()
+                print("%s:%d se ha conectado." % address)
+                print("Faltan " + str(faltan) + " conexiones")
+    print('Apagando')
+
+
+if __name__ == "__main__":
+    main()
